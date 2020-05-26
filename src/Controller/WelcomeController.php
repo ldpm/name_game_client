af4f4e26a82e0174582062bafb359e422b190004
ldpm <ajacs1@gmail.com>
2020-05-26 02:18:15 -0400

    Marking a name as complete and getting another is working. Kinda sloppy though; I can't get the Ajax button to work more than once, so we're going to and from the waiting room.

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -16,9 +16,20 @@ class WelcomeController extends ControllerBase {
    *   Return Hello string.
    */
   public function welcome() {
+    $tempstore = \Drupal::service('user.private_tempstore')->get('name_game_client');
+    $stored_hat = $tempstore->get('name_game_hat_id');
+    $stored_player = $tempstore->get('name_game_player_id');
+
+    $client = \Drupal::httpClient();
+    $request = $client->get('http://35.226.37.213/namegame/api/names/' . $stored_player);
+    if ($request->getStatusCode() == 200) {
+      $player = json_decode($request->getBody(), true);
+    }
+
     return [
-      '#type' => 'markup',
-      '#markup' => $this->t('Implement method: welcome')
+      '#theme' => 'waiting_room',
+      '#hat_id' => $stored_hat,
+      '#player' => $player,
     ];
   }
 
